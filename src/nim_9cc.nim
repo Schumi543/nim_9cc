8155f8bfc124a19bf73e20449323731c368cd6bc
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-09-03 17:54:43 +0900

    print number

--- src/nim_9cc.nim
+++ src/nim_9cc.nim
@@ -1,7 +1,14 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-
-import nim_9ccpkg/submodule
+import os
+import strformat
 
 when isMainModule:
-  echo(getWelcomeMessage())
+  let argc = paramCount()
+  let argv = commandLineParams()
+
+  assert(argc == 1)
+
+  echo(".intel_syntax noprefix");
+  echo(".global main");
+  echo("main:");
+  echo(&"  mov rax, {argv[0]}");
+  echo("  ret");
