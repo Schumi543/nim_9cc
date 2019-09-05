cc1d12c31babf85fca5f033b40e0e8f8ce4791a8
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-09-05 14:12:58 +0900

    impl add and sub

--- src/nim_9cc.nim
+++ src/nim_9cc.nim
@@ -1,5 +1,6 @@
 import os
 import strformat
+import strutils
 
 when isMainModule:
   let argc = paramCount()
@@ -14,7 +15,25 @@ when isMainModule:
 
   let input: string = commandLineParams()[0]
 
-  echo &"""
-      mov rax, {input}
-      ret
-  """
+  var isFirstNum: bool = true
+  var line: string = "      "
+
+  for token in tokenize(input, {'+', '*'}):
+    if isFirstNum:
+      doAssert(not token.issep)
+      echo &"      mov rax, {token.token}"
+      isFirstNum = false
+    else:
+      if token.issep:
+        doAssert(line == "      ")
+        case token.token:
+          of "+":
+            line &= "add rax, "
+          of "*":
+            line &= "mov rax, "
+
+      else:
+        echo line & token.token
+        line = "    "
+
+  echo "  ret"
--- test.sh
+++ test.sh
@@ -19,5 +19,6 @@ try() {
 
 try 0 0
 try 42 42
+try 8 5+3
 
 echo OK
