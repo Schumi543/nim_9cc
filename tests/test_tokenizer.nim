5a3d6331a5d960d0a09b4a2e8ea2c9ff6786d5a2
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-09-09 22:53:22 +0900

    add test

---
+++ tests/test_tokenizer.nim
@@ -0,0 +1,14 @@
+import tokenizer
+import unittest
+import options
+
+suite "tokenize":
+    setup:
+        let input = "12,23,34"
+        let seps = {','}
+
+    test "head is desirable":
+        let t = tokenizer.tokenize(input, seps)
+
+        # check t.head[].value of
+        check t.head[].value[].str == some("12")
