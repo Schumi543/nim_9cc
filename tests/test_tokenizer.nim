1df9790486bc5594605f755980f58f37070079a9
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-09-14 11:35:28 +0900

    update test

--- tests/test_tokenizer.nim
+++ tests/test_tokenizer.nim
@@ -12,3 +12,26 @@ suite "tokenize":
 
         # check t.head[].value of
         check t.head[].value[].lexeme == some("12")
+
+suite "tokenize(iterator)":
+    test "":
+        let input = "12*20+10"
+        let seps = {'*', '+'}
+
+        var tokenized_input: seq[tuple[token: string, isSep: bool]] = @[]
+        for word in tokenize(input, seps):
+            tokenized_input.add(word)
+        check tokenized_input == @[(token: "12", isSep: false),
+         (token: "*", isSep: true), (token: "20", isSep: false),
+          (token: "+", isSep: true), (token: "10", isSep: false)]
+
+    test "":
+        let input = "12>=20+10"
+        let seps = {"+"}
+
+        var tokenized_input: seq[tuple[token: string, isSep: bool]] = @[]
+        for word in tokenize(input, seps):
+            tokenized_input.add(word)
+        check tokenized_input == @[(token: "12", isSep: false),
+         (token: "*", isSep: true), (token: "20", isSep: false),
+          (token: "+", isSep: true), (token: "10", isSep: false)]
